8236cd775ef4806d1e1717a7de49bd3c8e4c34d5
Alexandre Assuncao <alexandre.myemail@gmail.com>
2024-06-16 00:57:17 -0300

    JUnit5 - @MethodSource

--- src/test/java/com/example/demo/CalculadoraTest.java
+++ src/test/java/com/example/demo/CalculadoraTest.java
@@ -4,6 +4,11 @@ import com.example.demo.exceptions.InvalidParamException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,5 +34,27 @@ class CalculadoraTest {
         assertThrows(InvalidParamException.class, () -> {
             calculadora.subtrair(0, 10);
         });
+    }
+
+    @DisplayName("Testando subtrair da calculadora - Param")
+    @ParameterizedTest
+    @MethodSource // ("subtrairParam")
+    void subtrairParam(double paramUm, double paramDois, double esperado) {
+
+        var resultado = calculadora.subtrair(paramUm, paramDois);
+
+        // Com delta, garantindo 2 casas decimais
+        assertEquals(esperado, resultado, 2D);
+        assertNotEquals(200, resultado, 2D);
+    }
+
+    // Mesmo nome garantindo que vai ser chamado em @MethodSource [subtrairParam]
+    public static Stream<Arguments> subtrairParam() {
+        return Stream.of(
+                Arguments.of(50, 20, 30),
+                Arguments.of(100, 50, 50),
+                Arguments.of(10, 10, 0),
+                Arguments.of(100.5, 50.25, 50.25)
+        );
     }
 }

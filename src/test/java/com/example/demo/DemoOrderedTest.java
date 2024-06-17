cd996f6378789b0f80b20587e8b30b511af36b3b
Alexandre Assuncao <alexandre.myemail@gmail.com>
2024-06-17 14:57:47 -0300

    JUnit5 - @TestMethodOrder

---
+++ src/test/java/com/example/demo/DemoOrderedTest.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+//@TestMethodOrder(MethodOrderer.Random.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class DemoOrderedTest {
+
+    @Test
+    @Order(3)
+    void testA() {
+        System.out.println("TESTE A");
+    }
+
+    @Test
+    @Order(1)
+    void testB() {
+        System.out.println("TESTE B");
+    }
+
+    @Test
+    @Order(2)
+    void testC() {
+        System.out.println("TESTE C");
+    }
+}

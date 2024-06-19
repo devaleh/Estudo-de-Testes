83324754388886c58578fb89650745c455fbe902
Alexandre Assuncao <alexandre.myemail@gmail.com>
2024-06-19 01:22:42 -0300

    Mockito - when/then, add comentarios

--- src/test/java/com/example/demo/ListTest.java
+++ src/test/java/com/example/demo/ListTest.java
@@ -12,9 +12,13 @@ public class ListTest {
     @Test
     void testMockingList() {
 
+        // Mocando uma lista
         List<?> list = mock(List.class);
+
+        // Quando list.size() ser chamado, retornar 10
         when(list.size()).thenReturn(10);
 
+        // assert de comparação
         assertEquals(10, list.size());
     }
 }

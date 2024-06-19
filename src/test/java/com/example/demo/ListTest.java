da2a410b0526bcfdaf9e9fe344a259b8af7a9472
Alexandre Assuncao <alexandre.myemail@gmail.com>
2024-06-19 18:04:39 -0300

    Mockito - Multiplos then / ArgumentsMatcher

--- src/test/java/com/example/demo/ListTest.java
+++ src/test/java/com/example/demo/ListTest.java
@@ -21,4 +21,38 @@ public class ListTest {
         // assert de comparação
         assertEquals(10, list.size());
     }
+
+    @Test
+    void testMockingListMultiple() {
+
+        // Mocando uma lista
+        List<?> list = mock(List.class);
+
+        // Quando list.size() ser chamado, retornar 10
+        when(list.size()).thenReturn(10).thenReturn(20).thenReturn(30);
+
+        // assert de comparação
+        // Primeiro valor mocado
+        assertEquals(10, list.size());
+        // Segundo valor mocado
+        assertEquals(20, list.size());
+        // Terceiro valor mocado
+        assertEquals(30, list.size());
+        // Se repete o ultimo valor mocado ...
+        assertEquals(30, list.size());
+    }
+
+    @Test
+    void testMockingListArgumentsMatcher() {
+
+        // Mocando uma lista
+        var list = mock(List.class);
+
+        // Quando acessar qualquer posição de list retornara "Alexandre"
+        when(list.get(anyInt())).thenReturn("Alexandre");
+
+        // assert de comparação
+        assertEquals("Alexandre", list.get(1));
+        assertEquals("Alexandre", list.get(anyInt()));
+    }
 }

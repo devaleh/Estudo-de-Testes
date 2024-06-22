2adae27221323188655dcf001cb2db2eaad13856
Alexandre Assuncao <alexandre.myemail@gmail.com>
2024-06-22 00:34:04 -0300

    Mockito - spy / verify

---
+++ src/test/java/com/example/demo/SpyTest.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class SpyTest {
+
+    @Test
+    void testV1() {
+        // List<String> mockArrayList = mock(ArrayList.class);
+        List<String> mockArrayList = spy(ArrayList.class);
+        assertEquals(0, mockArrayList.size());
+
+        // when(mockArrayList.size()).thenReturn(5);
+        mockArrayList.add("Foo Bar");
+        // assertEquals(5, mockArrayList.size());
+        assertEquals(1, mockArrayList.size());
+        mockArrayList.remove("Foo Bar");
+        assertEquals(0, mockArrayList.size());
+    }
+
+    @Test
+    void testV2() {
+        List<String> mockArrayList = spy(ArrayList.class);
+        assertEquals(0, mockArrayList.size());
+
+        when(mockArrayList.size()).thenReturn(5);
+        assertEquals(5, mockArrayList.size());
+    }
+
+    @Test
+    void testV3() {
+        List<String> mockArrayList = spy(ArrayList.class);
+        mockArrayList.add("Foo Bar");
+
+        verify(mockArrayList).add("Foo Bar");
+        verify(mockArrayList, never()).remove(anyString());
+        verify(mockArrayList, never()).clear();
+    }
+
+}
